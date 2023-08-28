9201abb11980c03dfc4cd2f4dbad88525e0b66a8
denilson <denilson020898@gmail.com>
2023-08-29 06:47:40 +0700

    commit A
    squash
    
    squash daddy

---
+++ 02_echor/src/main.rs
@@ -0,0 +1,27 @@
+use clap::{App, Arg};
+
+// squash me daddy 2
+fn main() {
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Denilson <denilson020898@gmail.com>")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do no print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+}
