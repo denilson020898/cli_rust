88eec24c5666586e24ef6dc1934ecb497bab20fd
denilson <denilson020898@gmail.com>
2023-11-04 15:41:37 +0700

    start chapter 10

---
+++ 10_commr/src/lib.rs
@@ -0,0 +1,93 @@
+use std::error::Error;
+
+use clap::{App, Arg};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    file1: String,
+    file2: String,
+    show_col1: bool,
+    show_col2: bool,
+    show_col3: bool,
+    insensitive: bool,
+    delimiter: String,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("commr")
+        .version("0.1.0")
+        .author("Denilson <denilson020898@gmail.com>")
+        .about("Rust comm")
+        .arg(
+            Arg::with_name("file1")
+                .value_name("FILE1")
+                .help("Input file 1")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("file2")
+                .value_name("FILE2")
+                .help("Input file 2")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("delim")
+                .value_name("DELIM")
+                .long("output-delimiter")
+                .short("d")
+                .help("Output delimiter")
+                .takes_value(true)
+                .default_value("\t"),
+        )
+        .arg(
+            Arg::with_name("insensitive")
+                .short("i")
+                .help("Case-insensitive comparison of lines")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("show_col1")
+                .short("1")
+                .help("Suppress printing of column 1")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("show_col2")
+                .short("2")
+                .help("Suppress printing of column 2")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("show_col3")
+                .short("3")
+                .help("Suppress printing of column 3")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let file1 = matches.value_of("file1").unwrap().to_string();
+    let file2 = matches.value_of("file2").unwrap().to_string();
+    let delimiter = matches.value_of("delim").unwrap().to_string();
+
+    let show_col1 = matches.is_present("show_col1") || true;
+    let show_col2 = matches.is_present("show_col2") || true;
+    let show_col3 = matches.is_present("show_col3") || true;
+    let insensitive = matches.is_present("insensitive") || false;
+
+    Ok(Config {
+        file1,
+        file2,
+        show_col1,
+        show_col2,
+        show_col3,
+        insensitive,
+        delimiter,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
